c83de68168bf1045690c6c81a0527be1177990da
MITI-Apps <okekem186@gmail.com>
2026-09-18 18:49:53 +0100

    Chore: Started the server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Container Tracker API");
+import express from "express";
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Server is up and running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
+});
